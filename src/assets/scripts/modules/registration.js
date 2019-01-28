c4a34212f069e8cdfaa53a586c5222db33038bb2
Alexander Varnaev <alexandervarnaev@gmail.com>
2019-01-28 15:57:49 +0300

    onresize closeReg bug fixed

--- src/assets/scripts/modules/registration.js
+++ src/assets/scripts/modules/registration.js
@@ -2,20 +2,28 @@ var btn = document.querySelector(".button"),
     closeReg = document.querySelector(".registration__close"),
     regForm = document.querySelector(".registration"),
     iFrame = document.querySelector(".registration__frame");
+    
+var changePosition = function() {
+        var framePosition = iFrame.getBoundingClientRect(),
+            top = framePosition.top,
+            right = framePosition.left;
+
+        closeReg.style.top = top + "px";
+        closeReg.style.right = right + "px";
+    };
 
 btn.addEventListener("click", function(e) {
     e.preventDefault;
     regForm.style.display = "block";
 
-    var framePosition = iFrame.getBoundingClientRect(),
-        top = framePosition.top,
-        right = framePosition.left;
-
-    closeReg.style.top = top + "px";
-    closeReg.style.right = right + "px";
+    changePosition();
 });
 
 regForm.addEventListener("click", function(e) {
     e.preventDefault;
     regForm.style.display = "none";
 });
+
+window.onresize = function() {
+    changePosition();
+};
